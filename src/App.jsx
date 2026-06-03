b774eb816ce7a15bd6dfdd08f03f4b1253b40ff1
Mohammed Rakib <mohammedrakibhossain02@gmail.com>
2026-06-03 15:59:05 +0600

    add products and cart toggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Stats from "./components/Stats";
 import ProductsSection from "./sections/ProductsSection";
+import { useState } from "react";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("products");
+  const [cart, setCart] = useState([]);
   return (
     <>
       <Navbar />
       <Banner />
       <Stats />
-      <ProductsSection />
+      <ProductsSection
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        cart={cart}
+      />
     </>
   );
 }
--- src/sections/ProductsSection.jsx
+++ src/sections/ProductsSection.jsx
@@ -1,7 +1,8 @@
 import products from "../data/products.json";
 import ProductCard from "../components/ProductCard";
+import { BsCartPlus } from "react-icons/bs";
 
-const ProductsSection = () => {
+const ProductsSection = ({ activeTab, setActiveTab, cart }) => {
   return (
     <section className="max-w-7xl mx-auto px-4 lg:px-8 py-16">
       <div className="text-center">
@@ -14,24 +15,54 @@ const ProductsSection = () => {
           designed to boost your productivity and creativity.
         </p>
       </div>
-
       <div className="flex justify-center mt-8">
         <div className="bg-gray-100 p-1 rounded-full flex">
-          <button className="px-8 py-3 rounded-full bg-gradient-to-r from-purple-600 to-fuchsia-500 text-white font-semibold">
+          <button
+            onClick={() => setActiveTab("products")}
+            className={`px-8 py-3 rounded-full font-semibold transition-all
+      ${
+        activeTab === "products"
+          ? "bg-gradient-to-r from-purple-600 to-fuchsia-500 text-white"
+          : "text-slate-700"
+      }`}
+          >
             Products
           </button>
 
-          <button className="px-8 py-3 font-semibold text-slate-700">
-            Cart (0)
+          <button
+            onClick={() => setActiveTab("cart")}
+            className={`px-8 py-3 rounded-full font-semibold transition-all
+      ${
+        activeTab === "cart"
+          ? "bg-gradient-to-r from-purple-600 to-fuchsia-500 text-white"
+          : "text-slate-700"
+      }`}
+          >
+            Cart ({cart.length})
           </button>
         </div>
       </div>
+      {activeTab === "products" ? (
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="mt-12 flex flex-col items-center justify-center text-center">
+          <div className="w-34 h-34 rounded-full bg-purple-100 flex items-center justify-center">
+            <BsCartPlus className="text-7xl text-purple-600" />
+          </div>
 
-      <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+          <h3 className="mt-6 text-2xl font-bold text-slate-800">
+            Your Cart Is Empty
+          </h3>
+
+          <p className="mt-2 text-gray-500 max-w-sm">
+            Add products to your cart and they will appear here.
+          </p>
+        </div>
+      )}
     </section>
   );
 };
